9a798b1872054d856b8249208827836a49a07e62
EvaTu <evatuw@gmail.com>
2022-07-13 11:42:34 -0400

    added project 1 - vanilla JS

---
+++ project_one/main.js
@@ -0,0 +1,9 @@
+
+let num1 = document.getElementById('num1');
+let num2 = document.getElementById('num2');
+
+let addButton = document.getElementsByClassName(button)
+
+addButton.addEventListener('click', function(){
+    console,log('addButton clicked')
+})

ed517da253173a13cfcc062c8f39efaaa40d6158
EvaTu <evatuw@gmail.com>
2022-07-18 23:59:16 -0400

    added section four review JS

---
+++ project_five/app.js
@@ -0,0 +1,6 @@
+"use strict";
+var add = function (a, b) {
+    if (b === void 0) { b = 5; }
+    return a + b;
+};
+console.log(add(10));
---
+++ project_five/app.ts
@@ -0,0 +1,3 @@
+const add = (a: number, b: number = 5) => a + b
+
+console.log(add(10))
